944b06196f10108eee38b9eb85b26adf656095c8
emersonleo <emerson.leonardo@ufrpe.br>
2020-01-08 01:22:28 -0300

    primeiros passos com Jquery

---
+++ trocarStatustxt.js
@@ -0,0 +1,16 @@
+$(document).ready(function(){
+	$('#btn2').hide()
+	$('#Exibirtxt').hide()
+	$('#btn').click(function(){
+		$('#h1').hide()
+		$('#btn').hide()
+		$('#Exibirtxt').show()
+		$('#btn2').show()
+	})
+	$('#btn2').click(function(){
+		$('#btn2').hide()
+		$('#Exibirtxt').hide()
+		$('#h1').show()
+		$('#btn').show()
+	})
+})
